6a68a20af26e4d19ef446db357da43f29db460d1
Smitt86 <pavlov1086@ya.ruq>
2023-07-25 22:45:02 +0300

    Created method for original array

--- certification_task/Program.cs
+++ certification_task/Program.cs
@@ -1,3 +1,26 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из 
 // строк, длина которых меньше, либо равна 3 символам.
 
+using System;
+using static System.Console;
+
+Clear();
+
+string[] array = new string[3];
+GetArray(array);
+PrintArray(array);
+
+void GetArray(string[] arr)
+{
+    int i;
+    Console.WriteLine("Введите значения массива");
+    for (i = 0; i < arr.Length; i++)
+        arr[i] = Console.ReadLine()!;
+}
+
+void PrintArray(string[] arr)
+{
+    int i;
+    for (i = 0; i < arr.Length; i++)
+        Write($"{arr[i]}, ");
+}
